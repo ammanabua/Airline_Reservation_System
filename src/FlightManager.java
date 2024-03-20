e564db32643e4c2f813d4f59c52f15a55590927d
Amman Abua <ammanabua@gmail.com>
2024-03-20 18:25:24 -0500

    add flight manager class

---
+++ src/FlightManager.java
@@ -0,0 +1,46 @@
+import java.sql.Array;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class FlightManager {
+    String flightManagerId;
+
+    private List<Flight> flights;
+    private List<Booking> bookedTravellers;
+
+    public FlightManager(String flightManagerId){
+        this.flightManagerId = flightManagerId;
+        flights = new ArrayList<Flight>();
+        bookedTravellers = new ArrayList<Booking>();
+    }
+
+    public String getFlightManagerId() {
+        return flightManagerId;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public List<Booking> getBookedTravellers() {
+        return bookedTravellers;
+    }
+
+    public void addFlight(Flight f) {
+        flights.add(f);
+    }
+
+    public Flight getFlight(String flightId){
+        Flight flight = null;
+        for(Flight f: flights){
+            if(f.getFlightId().equals(flightId)){
+                flight = f;
+            }
+            else {
+                System.out.println("Flight doesn't exist in the logs");
+                return null;
+            }
+        }
+        return flight;
+    }
+}
