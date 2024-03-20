9660ff553f4b42faa7ca8b77953bc5e967eba312
BtsuanEyakem <btsuanme@gmail.com>
2024-03-20 18:29:07 -0500

    add operator enum

--- src/Booking.java
+++ src/Booking.java
@@ -1,7 +1,12 @@
+import jdk.jfr.FlightRecorder;
+
 public class Booking {
     private String bookingCode;
     private Passenger passenger;
+    private String destination;
+    private String Origin;
 
+    private Ticket generateTicket
 
 }
 
