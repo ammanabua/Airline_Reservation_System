4c43de1d9a131a664d55cc81376d5b9ae1e1d58f
BtsuanEyakem <btsuanme@gmail.com>
2024-03-20 18:10:19 -0500

    update class

--- src/Booking.java
+++ src/Booking.java
@@ -2,8 +2,6 @@ public class Booking {
     private String bookingCode;
     private Passenger passenger;
 
-    public Booking(String bookingCode, Passenger passenger) {
-        this.bookingCode = bookingCode;
-        this.passenger = passenger;
-    }
+
 }
+
