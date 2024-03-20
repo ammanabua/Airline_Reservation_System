ad05aab518fbe038b08bf87d085b8d682e21c273
BtsuanEyakem <btsuanme@gmail.com>
2024-03-20 18:40:08 -0500

    updated classes

--- src/Booking.java
+++ src/Booking.java
@@ -5,8 +5,42 @@ public class Booking {
     private Passenger passenger;
     private String destination;
     private String Origin;
+    private FlightManager flightManager;
 
-    private Ticket generateTicket
+    public Booking(String bookingCode, Passenger passenger, String destination, String origin, FlightManager flightManager) {
+        this.bookingCode = bookingCode;
+        this.passenger = passenger;
+        this.destination = destination;
+        Origin = origin;
+        this.flightManager = flightManager;
+
+    }
+
+    public String getBookingCode() {
+        return bookingCode;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getOrigin() {
+        return Origin;
+    }
+
+    public FlightManager getFlightManager() {
+        return flightManager;
+    }
+
+
+
+    private Ticket generateTicket(){
+
+    }
 
 }
 
