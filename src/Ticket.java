53ce858ced9187f68a16699565a3f5d6c802ae2c
Amman Abua <ammanabua@gmail.com>
2024-03-20 19:16:17 -0500

    update classes and run app

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,16 +1,16 @@
 public class Ticket {
     private String ticketId;
-    private Booking bookingReference;
-    private int numOfSeats;
+    private String bookingReference;
+    private String seatNo;
     private Passenger passenger;
     private Flight flightDetails;
     private Category seatClass;
     public Ticket() {}
 
-    public Ticket(String ticketId, Booking bookingReference, int numOfSeats, Passenger passenger, Flight flightDetails, Category seatClass) {
+    public Ticket(String ticketId, String bookingReference, String seatNo , Passenger passenger, Flight flightDetails, Category seatClass) {
         this.ticketId = ticketId;
         this.bookingReference = bookingReference;
-        this.numOfSeats = numOfSeats;
+        this.seatNo = seatNo;
         this.passenger = passenger;
         this.flightDetails = flightDetails;
         this.seatClass = seatClass;
@@ -20,12 +20,12 @@ public class Ticket {
         return ticketId;
     }
 
-    public Booking getBookingReference() {
+    public String getBookingReference() {
         return bookingReference;
     }
 
-    public int getNumOfSeats() {
-        return numOfSeats;
+    public String getSeatNo() {
+        return seatNo;
     }
 
     public Passenger getPassenger() {
